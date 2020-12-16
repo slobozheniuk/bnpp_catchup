1e7f9ea87cc9c7bebb94909fe37497a0bfacd27d
Евгений Слобоженюк <slobozhenuke@gmail.com>
2020-12-17 02:38:29 +0300

    Start HashMap

---
+++ HashMap/src/main/java/HashMap.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Evgeniy Slobozheniuk on 17-Dec-20.
+ */
+public class HashMap<K, V> {
+    public V get(K key) {
+
+    }
+
+    public void put(K key, V value) {
+
+    }
+
+    public void remove(K key) {
+
+    }
+}
